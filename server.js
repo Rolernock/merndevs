684dc458828ea312c36578079fdb4c8f38f2543f
Rolernock <90417353+Rolernock@users.noreply.github.com>
2024-10-07 20:06:38 +0300

    changed server port number

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ import { notFound, errorHandler } from './middleware/errorHandler.js'
 
 dotenv.config()
 const app = express()
-const PORT = 5000
+const PORT = process.env.PORT
 
 //Parse Incoming Request With JSON bodies
 app.use(express.json())
